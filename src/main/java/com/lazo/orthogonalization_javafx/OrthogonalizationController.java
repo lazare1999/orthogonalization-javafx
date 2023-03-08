f86b07159e01d45c0ce45de89f139afbc1cc44ef
lazo <lazarekvirtia@gmail.com>
2023-03-08 15:43:20 +0400

    weights

--- src/main/java/com/lazo/orthogonalization_javafx/OrthogonalizationController.java
+++ src/main/java/com/lazo/orthogonalization_javafx/OrthogonalizationController.java
@@ -3,16 +3,21 @@ package com.lazo.orthogonalization_javafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
-import static com.lazo.orthogonalization_javafx.algorithm.algorithm.ortAlgorithm;
+import static com.lazo.orthogonalization_javafx.algorithm.algorithm.*;
 
 public class OrthogonalizationController {
 
     public TextArea matrix;
     public TextArea rValues;
+    public Label y_matrix;
+    public Label weights;
+    public TextField element;
 
     @FXML
     private Label computeText;
@@ -25,14 +30,16 @@ public class OrthogonalizationController {
             var rV = getRv();
             if (rV ==null) {
                 computeText.setText("შეიყვანეთ საიმედოობები");
+                return;
             }
 
             var listX = createListx();
             if (listX ==null) {
                 computeText.setText("შეიყვანეთ მატრიცა");
+                return;
             }
 
-            var algAns = ortAlgorithm(listX, rV);
+            var algAns = ortAlgorithm_compute(listX, rV);
 
             computeText.setText(String.valueOf(algAns));
         } catch (Exception e) {
@@ -42,6 +49,55 @@ public class OrthogonalizationController {
 
 
 
+    }
+
+    @FXML
+    protected void printMatrix() {
+        try {
+
+
+            var listX = createListx();
+            if (listX ==null) {
+                y_matrix.setText("შეიყვანეთ მატრიცა");
+                return;
+            }
+
+            var algAns = ortAlgorithm_y(listX);
+
+            StringBuilder text = new StringBuilder();
+            for (var a : algAns) {
+
+                StringBuilder row = new StringBuilder();
+                for (var aa : a.entrySet()) {
+
+                    if (aa.getValue())
+                        row.append(aa.getKey()).append("  ");
+                    else
+                        row.append(aa.getKey()).append("`").append(" ");
+
+                }
+
+                text.append(row).append("\r\n");
+            }
+
+
+            y_matrix.setText(String.valueOf(text));
+        } catch (Exception e) {
+            y_matrix.setText("გაუთვალისწინებელი შეცდომა");
+        }
+
+    }
+
+    @FXML
+    protected void calculateWeights() {
+        var listX = createListx();
+        if (listX ==null) {
+            y_matrix.setText("შეიყვანეთ მატრიცა");
+            return;
+        }
+
+
+        weights.setText(String.valueOf(calculateWeight(ortAlgorithm_y(listX), Integer.valueOf(element.getText()))));
     }
 
     private HashMap<Integer, Double> getRv() {
@@ -83,7 +139,7 @@ public class OrthogonalizationController {
 
 
         for (var r : rows) {
-            HashMap<Integer, Boolean> row = new HashMap<>();
+            LinkedHashMap<Integer, Boolean> row = new LinkedHashMap<>();
 
             var el = r.split(",");
             for (var ell : el) {
--- src/main/java/com/lazo/orthogonalization_javafx/algorithm/algorithm.java
+++ src/main/java/com/lazo/orthogonalization_javafx/algorithm/algorithm.java
@@ -2,24 +2,17 @@ package com.lazo.orthogonalization_javafx.algorithm;
 
 import java.util.*;
 
+import static java.lang.Math.pow;
+
 /**
  * Created by Lazo on 2021-12-08
  */
 
 public class algorithm extends OrtAlgorithmHelper {
 
-    public static Double ortAlgorithm(ArrayList<HashMap<Integer, Boolean>> listX, HashMap<Integer, Double> rValues) {
+    public static Double ortAlgorithm_compute(ArrayList<HashMap<Integer, Boolean>> listX, HashMap<Integer, Double> rValues) {
 
-        listX.sort(Comparator.comparing(a -> Collections.min(a.keySet())));
-        listX.sort(Comparator.comparing(HashMap::size));
-
-        List<ArrayList<HashMap<Integer, Boolean>>> kInvertedMatrixList = new ArrayList<>();
-
-        for (int i=0; listX.size()-1 > i; i++) {
-            kInvertedMatrixList.add(invertHashMap(listX.get(i)));
-        }
-
-        ArrayList<HashMap<Integer, Boolean>> ans = calculateLogicalAnds(listX, kInvertedMatrixList);
+        ArrayList<HashMap<Integer, Boolean>> ans = ortAlgorithm_y(listX);
 
         double intAns = 1.0;
         for (var a : ans) {
@@ -37,6 +30,40 @@ public class algorithm extends OrtAlgorithmHelper {
         intAns= intAns-1.0;
 
         return intAns;
+    }
+
+    public static ArrayList<HashMap<Integer, Boolean>> ortAlgorithm_y(ArrayList<HashMap<Integer, Boolean>> listX) {
+        listX.sort(Comparator.comparing(a -> Collections.min(a.keySet())));
+        listX.sort(Comparator.comparing(HashMap::size));
+
+        List<ArrayList<HashMap<Integer, Boolean>>> kInvertedMatrixList = new ArrayList<>();
+
+        for (int i=0; listX.size()-1 > i; i++) {
+            kInvertedMatrixList.add(invertHashMap(listX.get(i)));
+        }
+
+        return calculateLogicalAnds(listX, kInvertedMatrixList);
+    }
+
+    public static Double calculateWeight(ArrayList<HashMap<Integer, Boolean>> y, Integer index) {
+
+        if (index ==null)
+            return 0.0;
+
+        double p = 0.0;
+        double n = 0.0;
+        for (var a : y) {
+            if (!a.containsKey(index))
+                continue;
+
+            if (a.get(index))
+                p +=pow(2, -(a.size()-1));
+            else
+                n +=pow(2, -(a.size()-1));
+        }
+
+        return (p - n);
+
     }
 
 }
